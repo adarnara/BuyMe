086116b1ebdc54731696775cb5222c4e9cce4363
Frances Cortuna <francescortuna@gmail.com>
2024-05-05 17:27:53 -0400

    Fixed schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,23 +6,23 @@ USE mybuy;
 CREATE TABLE IF NOT EXISTS CustomerRep (
                                            CustomerRep_ID INT AUTO_INCREMENT,
                                            CustomerRep_login VARCHAR(15) NOT NULL UNIQUE,
-                                           email_address VARCHAR(50) NOT NULL UNIQUE,
-                                           password VARCHAR(255) NOT NULL,
-                                           salt VARCHAR(255) NOT NULL,
-                                           PRIMARY KEY(CustomerRep_ID)
-);
+    email_address VARCHAR(50) NOT NULL UNIQUE,
+    PASSWORD VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    PRIMARY KEY (CustomerRep_ID)
+    );
 
 
 CREATE TABLE IF NOT EXISTS Admin (
                                      Admin_ID INT AUTO_INCREMENT,
                                      admin_login VARCHAR(15) NOT NULL UNIQUE,
-                                     email_address VARCHAR(50) NOT NULL UNIQUE,
-                                     password VARCHAR(255) NOT NULL,
-                                     salt VARCHAR(255) NOT NULL,
-                                     CustomerRep_ID INT,
-                                     PRIMARY KEY(Admin_ID),
-                                     FOREIGN KEY (CustomerRep_ID) REFERENCES CustomerRep(CustomerRep_ID) ON UPDATE CASCADE ON DELETE SET NULL
-);
+    email_address VARCHAR(50) NOT NULL UNIQUE,
+    PASSWORD VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    CustomerRep_ID INT,
+    PRIMARY KEY (Admin_ID),
+    FOREIGN KEY (CustomerRep_ID) REFERENCES CustomerRep (CustomerRep_ID) ON UPDATE CASCADE ON DELETE SET NULL
+    );
 
 
 -- run with option-x on MACOS on dbeaver
@@ -39,7 +39,7 @@ BEGIN
 
     IF admin_count >= 1 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error: There can only be one admin.';
-    END IF;
+END IF;
 END$$
 
 DELIMITER ;
@@ -47,10 +47,9 @@ DELIMITER ;
 CREATE TABLE IF NOT EXISTS Category (
                                         Category_ID INT AUTO_INCREMENT,
                                         Category_Name VARCHAR(50),
-                                        Parent_Category_ID INT,
-                                        PRIMARY KEY (Category_ID),
-                                        FOREIGN KEY (Parent_Category_ID) REFERENCES Category(Category_ID) ON UPDATE CASCADE ON DELETE SET NULL
-) ENGINE=InnoDB;  -- Ensure you use InnoDB which supports FULLTEXT indexes
+    Parent_Category_ID INT,
+    PRIMARY KEY (Category_ID),
+    FOREIGN KEY (Parent_Category_ID) REFERENCES Category (Category_ID) ON UPDATE CASCADE ON DELETE SET NULL) ENGINE = InnoDB;  -- Ensure you use InnoDB which supports FULLTEXT indexes
 
 ALTER TABLE Category ADD FULLTEXT idx_category_ngram (Category_Name) WITH PARSER ngram;
 
@@ -59,12 +58,11 @@ ALTER TABLE Category ADD FULLTEXT idx_category_ngram (Category_Name) WITH PARSER
 CREATE TABLE IF NOT EXISTS Items (
                                      Item_ID INT AUTO_INCREMENT,
                                      brand VARCHAR(25),
-                                     name VARCHAR(50),
-                                     color VARCHAR(50),
-                                     Category_ID INT,
-                                     PRIMARY KEY (Item_ID),
-                                     FOREIGN KEY (Category_ID) REFERENCES Category(Category_ID) ON UPDATE CASCADE ON DELETE SET NULL
-) ENGINE=InnoDB; -- Make sure you use a storage engine that supports FULLTEXT indexes, such as InnoDB
+    name VARCHAR(50),
+    color VARCHAR(50),
+    Category_ID INT,
+    PRIMARY KEY (Item_ID),
+    FOREIGN KEY (Category_ID) REFERENCES Category (Category_ID) ON UPDATE CASCADE ON DELETE SET NULL) ENGINE = InnoDB; -- Make sure you use a storage engine that supports FULLTEXT indexes, such as InnoDB
 
 ALTER TABLE Items ADD FULLTEXT INDEX idx_items_ngram (brand, name) WITH PARSER ngram;
 
@@ -72,24 +70,25 @@ ALTER TABLE Items ADD FULLTEXT INDEX idx_items_ngram (brand, name) WITH PARSER n
 CREATE TABLE IF NOT EXISTS EndUser (
                                        User_Id INT AUTO_INCREMENT,
                                        endUser_login VARCHAR(15) NOT NULL UNIQUE,
-                                       email_address VARCHAR(50) NOT NULL UNIQUE,
-                                       password VARCHAR(255) NOT NULL,
-                                       salt VARCHAR(255) NOT NULL,
-                                       user_type ENUM('buyer', 'seller') NOT NULL DEFAULT 'buyer',
-                                       PRIMARY KEY(User_Id)
-);
+    email_address VARCHAR(50) NOT NULL UNIQUE,
+    PASSWORD VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    user_type ENUM ('buyer',
+                    'seller') NOT NULL DEFAULT 'buyer',
+    PRIMARY KEY (User_Id)
+    );
 
 CREATE TABLE IF NOT EXISTS Question (
                                         question_ID INT AUTO_INCREMENT,
                                         answer_text TEXT,
                                         question_text TEXT,
                                         CustomerRep_ID INT,
-                                        User_Id INT NOT NULL,
-                                        PRIMARY KEY(question_ID),
-                                        FULLTEXT(question_text, answer_text),
-                                        FOREIGN KEY (CustomerRep_ID) REFERENCES CustomerRep(CustomerRep_ID) ON UPDATE CASCADE ON DELETE SET NULL,
-                                        FOREIGN KEY (User_Id) REFERENCES EndUser(User_Id) ON UPDATE CASCADE ON DELETE SET NULL
-) ENGINE=InnoDB;
+                                        User_Id INT,
+                                        PRIMARY KEY (question_ID),
+    FULLTEXT (question_text,
+              answer_text),
+    FOREIGN KEY (CustomerRep_ID) REFERENCES CustomerRep (CustomerRep_ID) ON UPDATE CASCADE ON DELETE SET NULL,
+    FOREIGN KEY (User_Id) REFERENCES EndUser (User_Id) ON UPDATE CASCADE ON DELETE SET NULL) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS Auction (
                                        Auction_ID INT AUTO_INCREMENT,
@@ -100,63 +99,67 @@ CREATE TABLE IF NOT EXISTS Auction (
                                        Initial_Price DOUBLE,
                                        Minimum DOUBLE,
                                        Winner VARCHAR(50),
-                                       auction_status ENUM('active', 'completed', 'cancelled') NOT NULL DEFAULT 'active',
-                                       User_Id INT NOT NULL,
-                                       Item_ID INT NOT NULL,
-                                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                                       PRIMARY KEY(Auction_ID),
-                                       FOREIGN KEY (Item_ID) REFERENCES Items(Item_ID) ON UPDATE CASCADE ON DELETE CASCADE,
-                                       FOREIGN KEY (User_Id) REFERENCES EndUser(User_Id) ON UPDATE CASCADE
-);
+    auction_status ENUM ('active',
+                         'completed',
+                         'cancelled') NOT NULL DEFAULT 'active',
+    User_Id INT NOT NULL,
+    Item_ID INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (Auction_ID),
+    FOREIGN KEY (Item_ID) REFERENCES Items (Item_ID) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (User_Id) REFERENCES EndUser (User_Id) ON UPDATE CASCADE
+    );
 
 
 CREATE TABLE IF NOT EXISTS Alerts (
                                       Alert_ID INT AUTO_INCREMENT,
                                       User_ID INT NOT NULL,
                                       Message VARCHAR(255),
-                                      Status ENUM ('Unread', 'Read', 'Pending') NOT NULL DEFAULT 'Unread',
-                                      Auction_ID INT,
-                                      Item_Name VARCHAR(50) DEFAULT NULL,
-                                      Item_Brand VARCHAR(25) DEFAULT NULL,
-                                      Category_Name VARCHAR(255) DEFAULT NULL,
-                                      Color VARCHAR(255) DEFAULT NULL,
-                                      PRIMARY KEY (Alert_ID, User_ID),
-                                      FOREIGN KEY (User_ID) REFERENCES EndUser(User_Id) ON UPDATE CASCADE ON DELETE CASCADE,
-                                      FOREIGN KEY (Auction_ID) REFERENCES Auction(Auction_ID) ON UPDATE CASCADE ON DELETE CASCADE
+    Status ENUM ('Unread',
+                 'Read',
+                 'Pending') NOT NULL DEFAULT 'Unread',
+    Auction_ID INT,
+    Item_Name VARCHAR(50) DEFAULT NULL,
+    Item_Brand VARCHAR(25) DEFAULT NULL,
+    Category_Name VARCHAR(255) DEFAULT NULL,
+    Color VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (Alert_ID, User_ID),
+    FOREIGN KEY (User_ID) REFERENCES EndUser (User_Id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (Auction_ID) REFERENCES Auction (Auction_ID) ON UPDATE CASCADE ON DELETE CASCADE
     );
 
 
 
 CREATE TABLE IF NOT EXISTS Bid (
                                    Bid_ID INT AUTO_INCREMENT,
-                                   Bid_Date DATE DEFAULT CURRENT_DATE,
-                                   Bid_Time TIME DEFAULT CURRENT_TIME,
-                                   Bid_Amount DOUBLE,
-                                   MaxBidAmount DOUBLE DEFAULT NULL,
-                                   Auction_ID INT NOT NULL,
-                                   User_Id INT NOT NULL,
-                                   PRIMARY KEY(Bid_ID),
-                                   FOREIGN KEY (Auction_ID) REFERENCES Auction(Auction_ID) ON UPDATE CASCADE ON DELETE CASCADE,
-                                   FOREIGN KEY (User_Id) REFERENCES EndUser(User_Id) ON UPDATE CASCADE
-);
+                                   Bid_Date DATE DEFAULT (CURRENT_DATE),
+    Bid_Time TIME DEFAULT (CURRENT_TIME),
+    Bid_Amount DOUBLE,
+    MaxBidAmount DOUBLE DEFAULT NULL,
+    Auction_ID INT NOT NULL,
+    User_Id INT NOT NULL,
+    PRIMARY KEY (Bid_ID),
+    FOREIGN KEY (Auction_ID) REFERENCES Auction (Auction_ID) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (User_Id) REFERENCES EndUser (User_Id) ON UPDATE CASCADE
+    );
 
 
 
 CREATE TABLE IF NOT EXISTS ItemsSubattributes (
                                                   Item_ID INT,
                                                   subattribute VARCHAR(25),
-                                                  PRIMARY KEY (Item_ID, subattribute),
-                                                  FOREIGN KEY (Item_ID) REFERENCES Items(Item_ID) ON UPDATE CASCADE ON DELETE CASCADE
-);
+    PRIMARY KEY (Item_ID, subattribute),
+    FOREIGN KEY (Item_ID) REFERENCES Items (Item_ID) ON UPDATE CASCADE ON DELETE CASCADE
+    );
 
 CREATE TABLE IF NOT EXISTS Posts (
                                      Item_ID INT,
                                      User_Id INT,
                                      PRIMARY KEY (Item_ID, User_Id),
-                                     FOREIGN KEY (Item_ID) REFERENCES Items(Item_ID) ON UPDATE CASCADE ON DELETE CASCADE,
-                                     FOREIGN KEY (User_Id) REFERENCES EndUser(User_Id) ON UPDATE CASCADE
-);
+    FOREIGN KEY (Item_ID) REFERENCES Items (Item_ID) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (User_Id) REFERENCES EndUser (User_Id) ON UPDATE CASCADE
+    );
 
 
 
@@ -177,29 +180,29 @@ BEGIN
         RETURN s2_len;
     ELSEIF s2_len = 0 THEN
         RETURN s1_len;
-    END IF;
+END IF;
     WHILE j <= s2_len DO
             SET cv1 = CONCAT(cv1, CHAR(j)),
                 j = j + 1;
-        END WHILE;
+END WHILE;
     WHILE i <= s1_len DO
             SET s1_char = SUBSTRING(s1, i, 1), c = i, cv0 = CHAR(i), j = 1;
             WHILE j <= s2_len DO
                     SET c = c + 1;
                     IF s1_char = SUBSTRING(s2, j, 1) THEN
                         SET cost = 0; ELSE SET cost = 1;
-                    END IF;
+END IF;
                     SET c_temp = ORD(SUBSTRING(cv1, j, 1)) + cost;
                     IF c > c_temp THEN SET c = c_temp; END IF;
                     SET c_temp = ORD(SUBSTRING(cv1, j+1, 1)) + 1;
                     IF c > c_temp THEN
                         SET c = c_temp;
-                    END IF;
+END IF;
                     SET cv0 = CONCAT(cv0, CHAR(c)), j = j + 1;
-                END WHILE;
+END WHILE;
             SET cv1 = cv0, i = i + 1;
-        END WHILE;
-    RETURN c;
+END WHILE;
+RETURN c;
 END$$
 
 
@@ -212,10 +215,10 @@ BEGIN
     SET s1_len = LENGTH(s1), s2_len = LENGTH(s2);
     IF s1_len > s2_len THEN
         SET max_len = s1_len;
-    ELSE
+ELSE
         SET max_len = s2_len;
-    END IF;
-    RETURN ROUND((1 - LEVENSHTEIN(s1, s2) / max_len) * 100);
+END IF;
+RETURN ROUND((1 - LEVENSHTEIN(s1, s2) / max_len) * 100);
 END$$
 
 DELIMITER ;
@@ -227,3 +230,8 @@ INSERT INTO Admin (admin_login, email_address, password, salt)
 VALUES ("One_Admin", "onlyadmin@gmail.com", "e1d0253d7e5ce8c582aa07c01e5cdf6bbd4d97ed7edec1e3921d469e77b0ea7f", "9fcb340a561f0d91148e068d544d94de");
 
 
+INSERT INTO Category (Category_Name)
+VALUES ("Electronics");
+
+INSERT INTO Category (Category_Name, Parent_Category_ID)
+VALUES ("Laptop", 1), ("Tablet", 1), ("Desktop", 1);
